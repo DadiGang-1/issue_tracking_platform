d6262cf8d1a4cc58714039e6771543108aa26ab5
David Tomas <105463065+DadiGang-1@users.noreply.github.com>
2025-10-14 09:53:40 +0000

    new function

--- start/start.php
+++ start/start.php
@@ -1,3 +1,14 @@
 <?php
-echo "stop watching !";
+function repondre_oui_non($phrase) {
+    $answer = readline("(o/n) $phrase ");
+    if ($answer == "o") {return true;} 
+    else if ($answer == "n"){return false;} 
+    else {return repondre_oui_non($phrase);}
+}
+
+if (repondre_oui_non("Voulez-vous continuer ?")) {
+    echo "On continue!\n";
+} else {
+    echo "On s'arrête!\n";
+}
 ?>
